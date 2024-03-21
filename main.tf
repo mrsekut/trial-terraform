9b32f4a5311ed02bbe0095aa2e93f148f6f475df
mrsekut <k.cloudspider@gmail.com>
2024-03-21 13:23:46 +0900

    add ASG

--- main.tf
+++ main.tf
@@ -8,22 +8,35 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  max_size             = 10
+  min_size             = 2
+  vpc_zone_identifier  = data.aws_subnets.default.ids
 
-  user_data                   = <<-EOF
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  user_data_replace_on_change = true
 
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
+
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -34,4 +47,15 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
