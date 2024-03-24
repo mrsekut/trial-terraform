8e06dfe2b4fa93a87ae6b55bdb3185af37574750
mrsekut <k.cloudspider@gmail.com>
2024-03-24 15:40:19 +0900

    add tag

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -42,10 +42,14 @@ resource "aws_autoscaling_group" "example" {
   max_size = var.max_size
   min_size = var.min_size
 
-  tag {
-    key                 = "Name"
-    value               = "${var.cluster_name}-asg"
-    propagate_at_launch = true
+  dynamic "tag" {
+    for_each = var.custom_tags
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
   }
 }
 
